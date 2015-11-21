66a7671a25eca0a007c591e7f9bf6f9ba69e3402
Mykola Jakowlew <mykola.jakowlew@gmail.com>
2015-11-21 20:37:36 +0200

    Geolocation

--- app/src/main/java/com/nulp/ism/turismapplication/MainActivity.java
+++ app/src/main/java/com/nulp/ism/turismapplication/MainActivity.java
@@ -1,81 +1,39 @@
 package com.nulp.ism.turismapplication;
 
+import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import java.util.Date;
+import com.google.android.gms.location.places.personalized.HereContent;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
-    private LocationManager locationManager;
+
+    GoogleMap googleMap;
+    int time = 2;
+    int offset = 10;
     @Override
-    protected  void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        int time = 10; // який мінімальний час має пройти, що б дані по геолокації оновилися
-        int offset = 10;
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * time, offset, locationListener);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * time, offset, locationListener);
-
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        locationManager.removeUpdates(locationListener);
-    }
-    private LocationListener locationListener = new LocationListener() {
-
-        @Override
-        public void onLocationChanged(Location location) {
-            //тут писати код, який виконується, коли обєкт змістився на визначену дистанцію
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            // код який виконується при виключенні провайдера (GPS або WiFI)
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            // код який виконується при включенні провайдера (GPS або WiFI)
-            showLocation(locationManager.getLastKnownLocation(provider));
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-    };
-    private void showLocation(Location location) {
-        if (location == null)
-            return;
-        String str_1,str_2;
-        //str_1 - в ній зберігається  текстовому форматі широта і довгота і час за GPS
-        //str_2 - в ній зберігається  текстовому форматі широта і довгота і час за WiFi
-        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
-            str_1 = formatLocation(location);
-        } else if (location.getProvider().equals(
-                LocationManager.NETWORK_PROVIDER)) {
-            str_2 = formatLocation(location);
-        }
-    }
-
-    private String formatLocation(Location location) {
-        if (location == null)
-            return "";
-        return String.format(
-                "Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT",
-                location.getLatitude(), location.getLongitude(), new Date(
-                        location.getTime()));
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);//Geo
+        createMapView();
+        addMarker(0, 0, "Marker 1");
+        addMarker(40, 34, "Marker 2");
+        addMarker(28, 40, "Marker 3");
+        addMarker(64, 33, "Marker 4");
+        addMarker(-14, 36, "Marker 5");
     }
 
     @Override
@@ -99,4 +57,100 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void createMapView(){
+        /**
+         * Catch the null pointer exception that
+         * may be thrown when initialising the map
+         */
+        try {
+            if(null == googleMap){
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                        R.id.mapView)).getMap();
+
+                /**
+                 * If the map is still null after attempted initialisation,
+                 * show an error to the user
+                 */
+                if(null == googleMap) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error creating map", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
+    }
+
+    private void addMarker(double x, double y, String name){
+
+        /** Make sure that the map has been initialised **/
+        if(null != googleMap){
+            googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(x, y))
+                            .title(name)
+                            .draggable(true)
+            );
+        }
+    }
+
+    /** Geolacation **/
+
+    private void addMyPosition(Location location){
+
+        if(null != googleMap){
+            googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(location.getLatitude(),location.getLongitude()))
+                            .title("Me")
+                            .draggable(false)
+            );
+        }
+    }
+    private LocationManager locationManager;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * time, offset, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * time, offset, locationListener);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //тут писати код, який виконується, коли обєкт змістився на визначену дистанцію
+            addMyPosition(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // код який виконується при виключенні провайдера (GPS або WiFI)
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // код який виконується при включенні провайдера (GPS або WiFI)
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    };
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)) {
+            addMyPosition(location);
+        } else if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            addMyPosition(location);
+        }
+    }
+
+    /** Geolacation **/
 }
